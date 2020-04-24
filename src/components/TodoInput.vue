c22970c454d156fe2c014cbef4227a164a9de230
Anjihye <anji314@naver.com>
2020-04-24 22:14:14 +0900

    finish input

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,8 +1,11 @@
 <template>
 
-<div>
-<input type="text" v-model="newTodoItem">
-<button v-on:click="addTodo">추가</button>
+<div class="inputBox shadow">
+<input type="text" v-model="newTodoItem" placeholder="type what you have to do" v-on:keyup.enter="addTodo">
+<span class="addContainer" v-on:click="addTodo">
+<i class="addBtn fa fa-plus" aria-hidden="true"></i>
+
+</span>
 </div>
 </template>
 
@@ -14,15 +17,48 @@ data(){
     }
 },
 methods: {
+     //setItem()은 로컬 스토리지에 데이터를 추가하는 API 
+     // (키,값)
     addTodo(){
-       localStorage.setItem(this.newTodoItem,this.newTodoItem); 
-       //setItem()은 로컬 스토리지에 데이터를 추가하는 API 
-       // (키,값)
-    }
+        if(this.newTodoItem != ""){
+            var value = this.newTodoItem&&this.newTodoItem.trim(); //인풋 박스 앞뒤 공백 문자열 제거
+            localStorage.setItem(value,value);
+            this.clearInput();
+        }
+        
+    },
+    clearInput(){
+            this.newTodoItem=''; // 인풋 박스의 입력 값을 초기화
+        }
+      
 }
 }
 </script>
 
 <style>
+input:focus{
+    outline:none;
+}
+.inputBox{
+    background :white;
+    height:50px;
+    line-height:50px;
+    border-radius:5px;
+}
+.inputBox input{
+    border-style:none;
+    font-size: 0.9rem;
+}
+.addContainer{
+    float:right;
+    background:linear-gradient(to right,#6478FB,#8763FB);
+    display:inline-block;
+    width:3rem;
+    border-radius: 0 5px 5px 0;
+}
+.addBtn{
+    color:white;
+    vertical-align:middle;
+}
 
 </style>
